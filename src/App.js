6c8e0cea7d63475eaa4ae4383801e60b17a3fdb4
Bambooh <cdgaviria40@misena.edu.co>
2019-09-05 16:45:27 -0500

    Comienzo de proyecto, estilos

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      oz | React | Meow
+    </div>
+  );
+}
+
+export default App;
